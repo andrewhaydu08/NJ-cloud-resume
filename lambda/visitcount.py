1cfabaf084642391d707051bb0ca946e81e6a413
andrewhaydu08 <103969201+andrewhaydu08@users.noreply.github.com>
2023-03-26 15:33:42 -0400

    Update visitcount.py

--- lambda/visitcount.py
+++ lambda/visitcount.py
@@ -1,8 +1,8 @@
 import json
 import boto3
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('dynamodb', region_name='us-east-1')
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('cloud-resume-challenge')
 
 
 def lambda_handler(event, context):
